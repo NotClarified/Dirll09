73f5b40767ba723c2b769b7f185ba016ccb88332
JeonDae <james911256@gmail.com>
2023-10-12 15:42:49 +0900

    자동방향전환、 속도 추가、 줄 간격 리펙토링
    ２０２０１５００３８ 전대권

--- boy.py
+++ boy.py
@@ -7,13 +7,13 @@ from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDLK_LEFT, SDL_KEYUP
 def space_down(e):  # e = event
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
 
+
 def a_key_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
 
+
 def time_out(e):
     return e[0] == 'TIME_OUT'
-
-
 
 
 def right_down(e):
@@ -31,38 +31,47 @@ def left_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
 
 
-
 def left_up(e):
     print('LEFT_UP')
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
+
 class AutoRun:
     @staticmethod
     def enter(boy, e):
-        if boy.action == 0 or boy.action == 2: #시작시 왼쪽방향
+        if boy.action == 0 or boy.action == 2:  # 시작시 왼쪽방향
             boy.dir, boy.action = -1, 0
-        elif boy.action == 1 or boy.action == 3: # 시작시 오른쪽 방향
-            boy.dir, boy.action= 1, 1
+        elif boy.action == 1 or boy.action == 3:  # 시작시 오른쪽 방향
+            boy.dir, boy.action = 1, 1
         boy.auto_run_time = get_time()
         boy.frame = 0
         print('Auto Start')
         pass
+
     @staticmethod
-    def exit(boy,e):
+    def exit(boy, e):
         print('Auto Exit')
         pass
+
     @staticmethod
     def do(boy):
         if get_time() - boy.auto_run_time > 5:  # do에서 계속 시간 체크, 5초 경과 시 handle event 발생
             boy.state_machine.handle_event(('TIME_OUT', 0))
         boy.frame = (boy.frame + 1) % 8
-        boy.x += boy.dir * 5 * 0.3 # Run의 0.3배 속도
+        boy.x += boy.dir * 5 * 3  # Run의 3배 속도
+
+        if boy.x >= 780:
+            boy.dir, boy.action = -1, 0
+        elif boy.x <= 30:
+            boy.dir, boy.action = 1, 1
         pass
+
     @staticmethod
-    def draw(boy): # 소년의 속도 빨라지고, 크기 커짐, 좌우측 끝에서 방향 전환
+    def draw(boy):  # 소년의 속도 빨라지고, 크기 커짐, 좌우측 끝에서 방향 전환
         # 기본 boy : boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
         boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y, 100 * 1.3, 100 * 1.3)
         pass
+
 
 class Sleep:
     @staticmethod
@@ -94,9 +103,9 @@ class Sleep:
 class Run:
     @staticmethod
     def enter(boy, e):
-        if right_down(e) or left_up(e):         # 오른쪽으로 RUN
-            boy.dir, boy.action = 1, 1          # 여기서의 dir은? 방향
-        elif left_down(e) or right_up(e):       # 왼쪽으로 RUN
+        if right_down(e) or left_up(e):  # 오른쪽으로 RUN
+            boy.dir, boy.action = 1, 1  # 여기서의 dir은? 방향
+        elif left_down(e) or right_up(e):  # 왼쪽으로 RUN
             boy.dir, boy.action = -1, 0
 
     @staticmethod
@@ -125,10 +134,12 @@ class Idle:
         boy.start_time = get_time()  # 경과시간을 가지고 오는 함수
         print('Idle Enter')
         pass
+
     @staticmethod
     def exit(boy, e):
         print('Idle Exit')
         pass
+
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
@@ -148,7 +159,7 @@ class StateMactine:
         self.cur_state = AutoRun
         self.transitions = {
             Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run,
-                time_out: Sleep, a_key_down: AutoRun},  # time_out event를 발생시키지 않음
+                   time_out: Sleep, a_key_down: AutoRun},  # time_out event를 발생시키지 않음
             Sleep: {right_down: Run, left_down: Run, left_up: Run, right_up: Run,
                     space_down: Idle, a_key_down: AutoRun},  # Sleep에서 space_down일경우 Idle로 감 -> handle_event에서 변환해야 함
             Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, a_key_down: AutoRun},
@@ -161,7 +172,8 @@ class StateMactine:
         pass
 
     def handle_event(self, e):
-        for check_event, next_state in self.transitions[self.cur_state].items():     # self의 테이블의 현재상태(sleep)에서의 itmes에서의 내용이 출력
+        for check_event, next_state in self.transitions[
+            self.cur_state].items():  # self의 테이블의 현재상태(sleep)에서의 itmes에서의 내용이 출력
             if check_event(e):
                 self.cur_state.exit(self.boy, e)  # 다음 상태로 가기 전 exit진행 # e를 추가한 이유 enter에 e를 전달해서 필요한 작업 수행
                 self.cur_state = next_state  # 다음 상태로 바꿈
