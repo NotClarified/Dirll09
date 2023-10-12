55a7f3419821277d92711247db6a01ae2176f2f3
JeonDae <james911256@gmail.com>
2023-10-12 10:49:23 +0900

    boy에서의 Sleep->Idle 상태로 가는 것 구현
    space 키 누르면 상태 변환

--- boy.py
+++ boy.py
@@ -1,7 +1,17 @@
 import math
 
 from pico2d import load_image
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE
 
+
+def space_down(e): # e = event
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
+
+def time_out_5(e): # 한번 만들어보는 5초 이벤트 타임아웃
+    return e[0] == 'TIME_OUT' and e[1] == 5.0
 class Sleep:
     @staticmethod
     def enter(boy):
@@ -41,10 +51,23 @@ class StateMactine:
     def __init__(self, boy):
         self.boy = boy
         self.cur_state = Sleep
+        self.table = {
+            Sleep: {space_down: Idle}, # Sleep에서 space_down일경우 Idle로 감 -> handle_event에서 변환해야 함
+            Idle: {time_out: Sleep} #time_out event를 발생시키지 않음 
+        }
         pass
     def start(self):
         self.cur_state.enter(self.boy)
         pass
+    def handle_event(self, e):
+        for check_event, next_state in self.table[self.cur_state].items(): # self의 테이블의 현재상태(sleep)에서의 itmes에서의 내용이 출력
+            if check_event(e):
+                self.cur_state.exit(self.boy) #다음 상태로 가기 전 exit진행
+                self.cur_state = next_state #다음 상태로 바꿈
+                self.cur_state.enter(self.boy) # 다음 상태에서의 entry action 수행
+                return True
+        return False # 들어온 이벤트에 대해 다음 상태로 못감을 반환 / 디버깅에 대해 유리함
+
     def update(self):
         self.cur_state.do(self.boy)
         pass
@@ -67,6 +90,8 @@ class Boy:
         self.state_machine.update()
 
     def handle_event(self, event):
+        print(event)
+        self.state_machine.handle_event(('INPUT', event))
         pass
 
     def draw(self):
